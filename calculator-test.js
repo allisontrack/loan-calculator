e9003471a380c2e0fefa9fc5e1829d069675d783
allisontrack <allisontrack@gmail.com>
2021-06-18 13:04:36 -0600

    commit before refactoring try-catch to unit test exceptions

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,23 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  expect(calculateMonthlyPayment({amount: 10000, years: 5, rate: 6})).toEqual('193.33');
+  expect(calculateMonthlyPayment({amount: 1, years: 1, rate: 1})).toEqual('0.08');
+  expect(calculateMonthlyPayment({amount: 0, years: 0, rate: 0})).toEqual('NaN');
+  expect(calculateMonthlyPayment({amount: -10000, years: 5, rate: 6})).toEqual('-193.33');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  // expect(calculateMonthlyPayment({amount: }))
+  expect(calculateMonthlyPayment({amount: 10000, years: 5, rate: 6})).toMatch(/[\d*][\.][\d{2}]/);
+  expect(calculateMonthlyPayment({amount: 1, years: 1, rate: 1})).toMatch(/[\d*][\.][\d{2}]/);
+
+
 });
 
-/// etc
+
+// it("should throw an exception for invalid inputs (non-numeric (all) and 0 (years, rate))", function() {
+//   expect(function() {
+//     calculateMonthlyPayment({amount: 10000, years: 0, rate: 6});
+//   }) .toThrow("Invalid Term in Years");
+// });
--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+
 window.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById("calc-form");
   if (form) {
@@ -9,32 +10,94 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 });
 
+// function getCurrentUIValues() {
+
+//     return {
+//       amount: +(document.getElementById("loan-amount").value), // does + make the input into an integer
+//       years: +(document.getElementById("loan-years").value),
+//       rate: +(document.getElementById("loan-rate").value),
+//     }
+
+// }
+
+
 function getCurrentUIValues() {
-  return {
-    amount: +(document.getElementById("loan-amount").value),
-    years: +(document.getElementById("loan-years").value),
-    rate: +(document.getElementById("loan-rate").value),
+
+  let inputAmount = +(document.getElementById("loan-amount").value);
+  let inputTerm = +(document.getElementById("loan-years").value);
+  let inputRate = +(document.getElementById("loan-rate").value);
+
+  try {
+    if (isNaN(inputAmount)) throw "Invalid Loan Amount entry.";
+    else if (isNaN(inputTerm) || inputTerm === 0) throw "Invalid Term in Years";
+    else if (isNaN(inputRate) || inputRate === 0) throw "Invalid Yearly Rate";
   }
+  catch(err) {
+    console.log(err);
+  }
+
+  return {
+    amount: inputAmount,
+    years: inputTerm,
+    rate: inputRate,
+  }
+
 }
 
 // Get the inputs from the DOM.
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+
+  document.getElementById("loan-amount").value = "10000";
+  document.getElementById("loan-years").value = "5";
+  document.getElementById("loan-rate").value = "6";
+
+  let values = getCurrentUIValues();
+
+  let monthlyPayment = calculateMonthlyPayment(values);
+  updateMonthly(monthlyPayment);
+  return monthlyPayment;
+
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+
+  let values = getCurrentUIValues();
+  let monthlyPaymentString = calculateMonthlyPayment(values);
+  updateMonthly(monthlyPaymentString);
+
 }
+
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+
+  // capture values for calculation, ensure values remain floating point values
+  let P = values.amount;
+  let i = (values.rate/100.0) / 12.0;
+  let n = values.years * 12.0;
+
+  // monthly payment calculation
+  let monthlyPayment = (P * i) / (1 - (1 + i) ** -n);
+
+  // converts monthly payment to a string with 2 decimal places
+  let monthlyPaymentFormatted = monthlyPayment.toFixed(2);
+
+  // updateMonthly(monthlyPaymentString);
+  return monthlyPaymentFormatted;
 }
+
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+
+  let monthlyPaymentDisplay = document.querySelector("#monthly-payment");
+  monthlyPaymentDisplay.innerText = `$${monthly}`;
+
 }
